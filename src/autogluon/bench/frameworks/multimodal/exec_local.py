ba75e3c87eb67f124524a8d2b8cee45d58087684
Ubuntu <ubuntu@ip-172-31-4-222.us-west-2.compute.internal>
2024-04-13 07:27:18 +0000

    no hf text insert sep

--- src/autogluon/bench/frameworks/multimodal/exec_local.py
+++ src/autogluon/bench/frameworks/multimodal/exec_local.py
@@ -608,9 +608,11 @@ if __name__ == "__main__":
     args.params['hyperparameters']["data.categorical.convert_to_text"] = args.categorical_convert_to_text
     args.params['hyperparameters']["data.categorical.convert_to_text_use_header"] = args.categorical_convert_to_text_use_header
     args.params['hyperparameters']["data.categorical.convert_to_text_use_header_template"] = args.categorical_convert_to_text_use_header_template
-    if args.categorical_convert_to_text_use_header_template == "latex":
-        assert args.no_hf_text_insert_sep == False
-        args.params['hyperparameters']["model.hf_text.insert_sep"] = False 
+     # if args.categorical_convert_to_text_use_header_template == "latex":
+    #     # assert args.no_hf_text_insert_sep == False
+    #     args.params['hyperparameters']["model.hf_text.insert_sep"] = False
+    if  args.no_hf_text_insert_sep == False:
+        args.params['hyperparameters']["model.hf_text.insert_sep"] = False  
     args.params['hyperparameters']["data.numerical.convert_to_text"] = args.numerical_convert_to_text
     args.params['hyperparameters']["data.numerical.convert_to_text_use_header"] = args.numerical_convert_to_text_use_header
     args.params['hyperparameters']["optimization.max_epochs"] = args.max_epochs
