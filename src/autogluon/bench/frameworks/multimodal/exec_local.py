06bcae5d7470a0f92899dddd656fb7657e231162
Ubuntu <ubuntu@ip-172-31-9-160.us-west-2.compute.internal>
2024-05-31 13:24:00 +0000

    img+text+tabular ensemble configs

--- src/autogluon/bench/frameworks/multimodal/exec_local.py
+++ src/autogluon/bench/frameworks/multimodal/exec_local.py
@@ -700,22 +700,51 @@ def run(
         else:
             prefix_str = f"ag_bench_runs/multimodal/{dataset_name}/top_k_average_method_greedy_soup/gradient_clip_val_1.0/warmup_steps_0.1/lr_schedule_cosine_decay/weight_decay_0.001/lr_decay_0.9/"
             all_configs = [
-                # # baseline+
-                # f"convert_to_text_False/ft_transformer_pretrained_False/auxiliary_weight_0.0/max_epochs_20/",
-                # # lf-transformer
-                # f"convert_to_text_False/ft_transformer_pretrained_False/use_fusion_transformer_True/auxiliary_weight_0.0/max_epochs_20/",
-                # # lf-aligned
-                # f"convert_to_text_False/ft_transformer_pretrained_False/use_clip_fusion_mlp/clip_fusion_mlp_quality_high/auxiliary_weight_0.0/max_epochs_20/",
-                # # early fusion
-                # f"convert_to_text_False/ft_transformer_pretrained_False/early_fusion_True/auxiliary_weight_0.0/max_epochs_20/",
-                # # lf-sequential fusion
-                # f"convert_to_text_False/ft_transformer_pretrained_False/sequential_fusion_True/auxiliary_weight_0.0/max_epochs_20/",
-                # # input aug
-                # f"convert_to_text_False/ft_transformer_pretrained_False/text_trivial_aug_maxscale_0.1/auxiliary_weight_0.0/max_epochs_20/",
-                # llama7B
-                f"convert_to_text_False/use_fusion_transformer_True/no_img_aug/epoch_20/fusion_transformer_concat_all_tokens_True/auxiliary_weight_0.0/use_llama7B_fusion/"
+                # baseline+
+                f"convert_to_text_False/no_img_aug/epoch_20/auxiliary_weight_0.0/",
+                # lf-transformer
+                f"convert_to_text_False/use_fusion_transformer_True/no_img_aug/epoch_20/auxiliary_weight_0.0/",
+                # lf-aligned
+                f"convert_to_text_False/no_img_aug/epoch_20/use_clip_fusion_mlp/clip_fusion_mlp_quality_high/auxiliary_weight_0.0/",
+                # lf-llm
+                f"convert_to_text_False/use_fusion_transformer_True/no_img_aug/epoch_20/fusion_transformer_concat_all_tokens_True/auxiliary_weight_0.0/use_llama7B_fusion/",
+                # early fusion
+                f"convert_to_text_False/no_img_aug/epoch_20/auxiliary_weight_0.0/early_fusion_True/",
+                # lf-sequential fusion
+                f"convert_to_text_False/no_img_aug/epoch_20/sequential_fusion/auxiliary_weight_0.0/",
+                # positive loss
+                f"convert_to_text_False/no_img_aug/epoch_20/auxiliary_weight_0.0/KL_feature_align_loss/",
+                # pos-neg loss
+                f"convert_to_text_False/no_img_aug/epoch_20/auxiliary_weight_0.0/contra_fea_contra_loss/contrastive_loss_w_1.0/",
+                # convert-categorical 
+                f"no_img_aug/epoch_20/auxiliary_weight_0.0/categorical_template_latex/no_hf_text_insert_sep_False/",
+                # convert numerical
+                "convert_to_text_False/no_img_aug/epoch_20/convert_to_text_numerical/",
+                # input aug
+                f"convert_to_text_False/epoch_20/auxiliary_weight_0.0/",
+                # fea independent aug
+                f"convert_to_text_False/no_img_aug/epoch_20/auxiliary_weight_0.0/manifold_mixup/",
+                # fea joint aug
+                f"convert_to_text_False/no_img_aug/epoch_20/auxiliary_weight_0.0/LeMDA/lemda_layer_6/",
+                
             ]
 
+            if dataset_name in ["petfinder", "covid-chestxray-dataset", "seattle_airbnb_convert_to_log", "KARD"]:
+                all_configs.append(
+                    # modality drop=0.3
+                    "convert_to_text_False/no_img_aug/epoch_20/auxiliary_weight_0.0/modality_drop_rate_0.3/"
+                )
+
+                if dataset_name in ["covid-chestxray-dataset", "seattle_airbnb_convert_to_log", ]:
+                    # miss embed
+                    all_configs.append(
+                        "convert_to_text_False/ft_transformer_pretrained_False/auxiliary_weight_0.0/max_epochs_20/use_miss_token_True/use_miss_token_True_numerical/"
+                    )
+                if dataset_name in ["KARD"]:
+                    # miss embed
+                    all_configs.append(
+                        "convert_to_text_False/ft_transformer_pretrained_False/auxiliary_weight_0.0/max_epochs_20/use_miss_token_True/use_miss_token_True_image/"
+                    )
 
         if params["seed"] == 0:
             seed_str = ""
