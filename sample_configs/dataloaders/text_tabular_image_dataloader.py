556078cc7a9c700b1446188dc967509a072a4b67
Ubuntu <ubuntu@ip-172-31-3-160.us-west-2.compute.internal>
2024-06-11 16:12:23 +0000

    text_tabular_image_dataloader.py

--- sample_configs/dataloaders/text_tabular_image_dataloader.py
+++ sample_configs/dataloaders/text_tabular_image_dataloader.py
@@ -22,76 +22,120 @@ class TextTabularImageDataLoader:
     def __init__(self, dataset_name: str, dataset_config_file: str, dataset_id: int = None, split: str = "train"):
         with open(dataset_config_file, "r") as f:
             config = yaml.safe_load(f)
-
         self.dataset_config = config[dataset_name]
-        if split == "val":
-            split = "validation"
-        if split not in self.dataset_config["splits"]:
-            logger.warning(f"Data split {split} not available.")
-            self.data = None
-            return
-        if split == 'test' and self.dataset_config['test_file'] == 'dev':
-            split = 'dev'
-
-        if self.dataset_config['data_folder']:
-            self.data_folder = self.dataset_config['data_folder']
-        else:
-            self.data_folder = f"{dataset_name}_processed"
         self.name = dataset_name
-        print(self.data_folder)
-        self.sub_dataset_name = None
-        if dataset_id is not None:
-            self.sub_dataset_name = self.dataset_config['datasets_id'][dataset_id]
-
-        self.sha1sum = self.dataset_config['sha1sum']
-        self.split = split
         self.feature_columns = self.dataset_config["feature_columns"]
         self.label_columns = self.dataset_config["label_columns"]
-        self.label_col = self.label_columns[0]
         self.ignore_columns = self.dataset_config["ignore_columns"]
-        self.image_columns = self.dataset_config["image_columns"]
 
-        url = self.dataset_config["url"]
-        file_name = url.split('/')[-1]
-        download_file_path = os.path.join(get_data_home_dir(), file_name)
-        download(url, download_file_path)
-        protected_zip_extraction(download_file_path, sha1_hash=self.sha1sum, folder=self.local_dir())
+        if dataset_name in ["fake", "airbnb", "channel", "qaa", "qaq", "cloth"]: # Text+Tabular datasets
+            if split not in self.dataset_config["splits"]:
+                logger.warning(f"Data split {split} not available.")
+                self.data = None
+                return
+            if split == 'test' and self.dataset_config['test_split_name'] == 'dev':
+                split = 'dev'
+            self.split = split
+            
 
-        if split == 'train':
-            self.data = pd.read_csv(os.path.join(self.base_folder(), self.dataset_config["train_file"]), sep=self.dataset_config['sep'])
-            if self.dataset_config["train_subset"]:
-                num_sample = self.dataset_config["train_subset"]
-                self.data = self.data.iloc[:num_sample]
-        elif split == 'validation':
-            self.data = pd.read_csv(os.path.join(self.base_folder(), self.dataset_config["val_file"]), sep=self.dataset_config['sep'])
-        elif split == 'test':
-            self.data = pd.read_csv(os.path.join(self.base_folder(), self.dataset_config["test_file"]), sep=self.dataset_config['sep'])
-            if self.dataset_config["test_subset"]:
-                num_sample = self.dataset_config["test_subset"]
-                self.data = self.data.iloc[:num_sample]
-        else:
-            raise NotImplementedError(f"Unsupported data split: {split}")
-        
-        if self.feature_columns is None:
-            self.feature_columns = list(self.data.columns.difference(self.ignore_columns + self.label_columns))
-        
-        if dataset_name != "mocheg" and dataset_name != "iqa" and dataset_name != "snli-ve":
+            url = self.dataset_config["url"].format(split=self.split)
+            file_extention = os.path.splitext(url)[-1]
+            base_dir = get_data_home_dir()
+            
+            self.data_path = os.path.join(base_dir, self.name, f'{split}{file_extention}')
+            download(url, path=self.data_path)
+            if file_extention == '.csv':
+                self.data = pd.read_csv(self.data_path)
+            elif file_extention == '.pq':
+                self.data = pd.read_parquet(self.data_path)
+            else:
+                raise NotImplementedError("Unsupported data type.")
+            
+            if self.feature_columns is None:
+                if self.ignore_columns is None:
+                    self.feature_columns = list(self.data.columns.difference(self.label_columns))
+                else:
+                    self.feature_columns = list(self.data.columns.difference(self.ignore_columns + self.label_columns))
+            _columns_to_drop = self.data.columns.difference(self.feature_columns + self.label_columns)
+            self.data.drop(columns=_columns_to_drop, inplace=True)
+        else: # Other datasets
+            with open(dataset_config_file, "r") as f:
+                config = yaml.safe_load(f)
+
+            self.dataset_config = config[dataset_name]
+            if split == "val":
+                split = "validation"
+            if split not in self.dataset_config["splits"]:
+                logger.warning(f"Data split {split} not available.")
+                self.data = None
+                return
+            if split == 'test' and self.dataset_config['test_file'] == 'dev':
+                split = 'dev'
+
+            if self.dataset_config['data_folder']:
+                self.data_folder = self.dataset_config['data_folder']
+            else:
+                print(self.dataset_config)
+                print(self.dataset_config["ori_name"])
+                print(self.dataset_config["ori_name"] == None)
+            
+                if self.dataset_config["ori_name"] != "":
+                    self.data_folder = f"{self.dataset_config['ori_name']}_processed"
+                else:
+                    self.data_folder = f"{dataset_name}_processed"
+            self.name = dataset_name
+
+            self.sub_dataset_name = None
+            if dataset_id is not None:
+                self.sub_dataset_name = self.dataset_config['datasets_id'][dataset_id]
+
+            self.sha1sum = self.dataset_config['sha1sum']
+            self.split = split
+            self.label_col = self.label_columns[0]
+            self.image_columns = self.dataset_config["image_columns"]
+
+            url = self.dataset_config["url"]
+            print("url: ", url)
+            file_name = url.split('/')[-1]
+            download_file_path = os.path.join(get_data_home_dir(), file_name)
+            print("get_data_home_dir(): ", get_data_home_dir())
+            download(url, download_file_path)
+            protected_zip_extraction(download_file_path, sha1_hash=self.sha1sum, folder=self.local_dir())
+
+            if split == 'train':
+                self.data = pd.read_csv(os.path.join(self.base_folder(), self.dataset_config["train_file"]), sep=self.dataset_config['sep'])
+                if self.dataset_config["train_subset"]:
+                    num_sample = self.dataset_config["train_subset"]
+                    self.data = self.data.iloc[:num_sample]
+            elif split == 'validation':
+                self.data = pd.read_csv(os.path.join(self.base_folder(), self.dataset_config["val_file"]), sep=self.dataset_config['sep'])
+            elif split == 'test':
+                self.data = pd.read_csv(os.path.join(self.base_folder(), self.dataset_config["test_file"]), sep=self.dataset_config['sep'])
+                if self.dataset_config["test_subset"]:
+                    num_sample = self.dataset_config["test_subset"]
+                    self.data = self.data.iloc[:num_sample]
+            else:
+                raise NotImplementedError(f"Unsupported data split: {split}")
+            
+            if self.feature_columns is None:
+                self.feature_columns = list(self.data.columns.difference(self.ignore_columns + self.label_columns))
+            
             for img_col in self.image_columns:
                 self.data[img_col].fillna("", inplace=True)
                 self.data[img_col] = self.data[img_col].apply(lambda ele: path_expander(ele, base_folder=self.base_folder()))
         
-        if self.dataset_config["convert_to_log"]:
-            print("convert_to_log ...")
-            for label_col in self.label_columns:
-                self.data = self.data.loc[self.data[label_col] > 0]
-                self.data[label_col] = np.log(self.data[label_col])
+            if self.dataset_config["convert_to_log"]:
+                print("convert_to_log ...")
+                for label_col in self.label_columns:
+                    self.data = self.data.loc[self.data[label_col] > 0]
+                    self.data[label_col] = np.log(self.data[label_col])
 
-        # filter data
-        use_all_images_datasets = ["id_change_detection", "grocery_image"]
-        if any([self.name.lower().startswith(per_dataset) for per_dataset in use_all_images_datasets]):
-            self.get_df_with_one_label()
-        else:
-            self.get_df_with_one_image_and_one_label()
+            # filter data
+            use_all_images_datasets = ["id_change_detection", "grocery_image"]
+            if any([self.name.lower().startswith(per_dataset) for per_dataset in use_all_images_datasets]):
+                self.get_df_with_one_label()
+            else:
+                self.get_df_with_one_image_and_one_label()
 
     @property
     def problem_type(self):
